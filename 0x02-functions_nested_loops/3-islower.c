6be13c7cd12e70b3c8a4b30a83007a13de1ed9e3
Brunorainz <oparabruno77@gmail.com>
2022-09-13 09:32:24 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * _islower - a function that checks for for lower character
+ *
+ * Return: 1 if int c is lowercase, 0 if otherwise
+*/
+int _islower(int c)
+{
+if 
+(
+c >= '0' && c <= '2'
+);
+return (0);
+else
+return (1);
+}
+
