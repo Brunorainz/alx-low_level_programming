87ebc3fd3ff072866cfd2a075cd85e842db852d2
Brunorainz <oparabruno77@gmail.com>
2022-09-22 08:15:59 -0700

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
- * @str: parameter
- * Return: string
+ * string_toupper - changes all lowercase letters to uppercase
+ * @str: string to be changed
+ *
+ * Return: address to the string
  */
 char *string_toupper(char *)
 {
-	int index = 0;
+	int i = 0;
 
-	while (str[index++])
+	while (str[i] != '\0')
 	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] -= 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
 		i++;
 	}
 	return (str);
