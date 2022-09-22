bdd82a85d50315f31904d5a2bc1a1cf38adbc547
Brunorainz <oparabruno77@gmail.com>
2022-09-22 08:02:29 -0700

    5. Always look up

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
 /**
- * leet - a leet function
- * @x: param x
- * Return: a string
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: address of s
  */
 char *leet(char *)
 {
-	int a = 0, b, l = 5;
-	char tr[5]5 = {'A', 'E', 'O', 'T', 'L'};
-	char trw[5] = {'4', '3', '0', '7', '1'};
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-	while (x[a])
+	for (i = 0; *(s + i); i++)
 	{
-		b = 0;
-
-		while (b < l)
+		for (j = 0; j <= 9; j++)
 		{
-			if (x[a] == tr[b] || x[a] - 32 == tr[b])
-				x[a] = trw[b];
-			b++;
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
 		}
-		a++;
 	}
-	return (x);
+	return (s);
 }
