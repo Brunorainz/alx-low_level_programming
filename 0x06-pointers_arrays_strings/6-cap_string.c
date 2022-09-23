cc52d7d5a800e3a22d0d646afff5710e243559ea
Brunorainz <oparabruno77@gmail.com>
2022-09-23 06:37:25 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -41,7 +41,7 @@ char *cap_string(char *str)
 				str[index - 1] == '(' ||
 				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
+				str[index - 1] == '}')
 			str[index] -= 32;
 	}
 
