cc8301fc3eeb61fea4b576d59f582fa87ea75206
Brunorainz <oparabruno77@gmail.com>
2022-09-27 06:34:50 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,9 +3,9 @@
 /**
  * _strstr - locates a substring
  * @haystack: string in which to check for needle
- * @needle: substring to find in hatstack
+ * @needle: substring to find in haystack
  *
- * Return: pointer to begining of needle in haystack or NULL if no match
+ * Return: pointer to beginning of needle in haystack or NULL if no match
  */
 char *_strstr(char *haystack, char *needle)
 {
