54d306b07eac4ac0c3e98f0d8572d8a2ce206547
Brunorainz <oparabruno77@gmail.com>
2022-09-28 08:05:34 -0700

    3. You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,14 +4,14 @@
  * factorial - returns the factorial of a given munber.
  * @n: the number to fnd the factorial of.
  *
- * Return: if n > 0 - the factorial of n.
- * if n < 0 - 1 to indicate an error.
+ * Return: factorial of number
+ * -1 if n < 0.
  */
 int factorial(int n)
 {
-int result = n;
-if (n < 0)
-return (-1);
-else if (n >= 0 && n <= 1)
-return (1);
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (1);
+	return (n * factorial(n - 1));
 }
