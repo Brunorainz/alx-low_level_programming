276fc6224fe0526e72409b65c2dd7452b2e1651f
Brunorainz <oparabruno77@gmail.com>
2022-09-28 05:48:27 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line.
+ * @s: the string to br printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
